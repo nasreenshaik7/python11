7a450c0fc6eb4f132f8c767b945c1f53fcafd98d
nasreenshaik7 <118714043+nasreenshaik7@users.noreply.github.com>
2022-11-28 12:32:15 +0530

    Add files via upload

---
+++ pythonas (1).py
@@ -0,0 +1,434 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "e46fb284",
+   "metadata": {},
+   "source": [
+    "# Assaignment"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "ed405470",
+   "metadata": {},
+   "source": [
+    "Q1. Why do we call Python as a general purpose and high-level programming\n",
+    "language?\n",
+    "\n",
+    "ans:Python is scripting, general-purpose, high-level, and interpreted       programming language.\n",
+    "It also provides the object-oriented programming approach.\n",
+    "high-level means it's easy for humans to understand.\n",
+    "\n",
+    " "
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "d46726cc",
+   "metadata": {},
+   "source": [
+    "Q2. Why is Python called a dynamically typed language?\n",
+    "\n",
+    "ans: Dynamic typing means that the type of the variable is determined only during runtime."
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "fb8cb04c",
+   "metadata": {},
+   "source": [
+    "Q3. List some pros and cons of Python programming language?\n",
+    "pros:\n",
+    "It's Simple.\n",
+    "It's Free.\n",
+    "It's Easy to Use.\n",
+    "It's Highly Compatible.\n",
+    "It is Object-Oriented.\n",
+    "It has Lots of Libraries.\n",
+    "It has Built-in Data Structures.\n",
+    "It's Widely Applicable.\n",
+    "\n",
+    "cons:\n",
+    "\n",
+    "=>Speed Limitations.\n",
+    "=>We have seen that Python code is executed line by line.\n",
+    "=>Weak in Mobile Computing and Browsers. While it serves as an excellent \n",
+    "=>server-side language, Python is much rarely seen on the client-side.\n",
+    "=>Design Restrictions.\n",
+    "=>Underdeveloped Database Access Layers.\n"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "d6e3e5c7",
+   "metadata": {},
+   "source": [
+    "Q4. In what all domains can we use Python?\n",
+    "ans: \n",
+    "1.Web development. Any business must invest in web development.\n",
+    "2.Data science. Python is an excellent tool for creating data-driven solutions. \n",
+    "3.OS development.\n",
+    "4.Scientific programming.\n",
+    "5.Gaming."
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "d503e0c8",
+   "metadata": {},
+   "source": [
+    "Q5. What are variable and how can we declare them?\n",
+    "\n",
+    "A Python variable is a symbolic name that is a reference or pointer to an object. Once an object is assigned to a variable, you can refer to the object by that name. But the data itself is still contained within the object. \n",
+    "The data type of the variable will be automatically determined from the value assigned, we need not define it explicitly.For example: a=10"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "ac3ff3fe",
+   "metadata": {},
+   "source": [
+    "Q6. How can we take an input from the user in Python?\n",
+    "ans:\n",
+    " In Python, Using the input() function, we take input from a user, and using the print() function, we display output on the screen. Using the input() function, users can give any information to the application in the strings or numbers format."
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "e03f2cb7",
+   "metadata": {},
+   "source": [
+    "Q7. What is the default datatype of the value that has been taken as an input using input() function?\n",
+    "\n",
+    "ans:the default datatype of the value that has been taken as an input using the input() function by default returns the value as string data type."
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "f3a8cadb",
+   "metadata": {},
+   "source": [
+    "Q8. What is type casting?\n",
+    "ans:\n",
+    "  The conversion of one data type into the other data type is known as type casting in python or type conversion in python. Python supports a wide variety of functions or methods like: int(), float(), str(), ord(), hex(), oct(), tuple(), set(), list(), dict(), etc. for the type casting in python."
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "dcca1547",
+   "metadata": {},
+   "source": [
+    "Q9. Can we take more than one input from the user using single input() function? If yes, how? If no, why?\n",
+    "\n",
+    "ans:No, python user input() function takes only one argument."
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "b43742ba",
+   "metadata": {},
+   "source": [
+    "Q10. What are keywords?\n",
+    "ans:\n",
+    " Python keywords are special reserved words that have specific meanings and purposes and can't be used for anything but those specific purposes. These keywords are always available—you'll never have to import them into your code. Python keywords are different from Python's built-in functions and types."
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "e0ae2e4b",
+   "metadata": {},
+   "source": [
+    "Q11. Can we use keywords as a variable? Support your answer with reason.\n",
+    "ans:\n",
+    " We cannot use a keyword as a variable name, function name, or any other identifier. They are used to define the syntax and structure of the Python language. All the keywords except True , False and None are in lowercase and they must be written as they are."
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "5c1d370e",
+   "metadata": {},
+   "source": [
+    "Q12. What is indentation? What's the use of indentaion in Python?\n",
+    "\n",
+    "Indentation refers to the spaces at the beginning of a code line. Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important. Python uses indentation to indicate a block of code."
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "5e7bc8b9",
+   "metadata": {},
+   "source": [
+    "Q13. How can we throw some output in Python?\n",
+    "\n",
+    "The basic way to do output is the print statement.In Python, the print() function is used to get the output and debug the code. To end the printed line with a newline, add a print statement without any objects. This will print to any object that implements write(), which includes file objects.\n"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "4d10a393",
+   "metadata": {},
+   "source": [
+    "Q14. What are operators in Python?\n",
+    "\n",
+    " an operator is a character or characters that determine the action that is to be performed or considered.Operators are used to perform operations on variables and values.operators in python: \n",
+    "Arithmetic operators\n",
+    "Assignment operators\n",
+    "Comparison operators\n",
+    "Logical operators\n",
+    "Identity operators\n",
+    "Membership operators\n",
+    "Bitwise operators\n"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "b9e7872b",
+   "metadata": {},
+   "source": [
+    "Q15. What is difference between / and // operators?\n",
+    "\n",
+    "Float Division(\"/\"): Divides left hand operand by right hand operand.Division works in Python the way it's mathematically defined.\n",
+    "\n",
+    "Floor Division(\"//\"): The division of operands where the result is the quotient in which the digits after the decimal point are removed. But if one of the operands is negative, the result is floored , i.e., rounded away from zero"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "df713a2a",
+   "metadata": {},
+   "source": [
+    "#Q16. Write a code that gives following as an output.\n",
+    "# iNeuroniNeuroniNeuroniNeuron\n",
+    "def string(a,b):\n",
+    "    return a*b\n",
+    "a=input(\"enter string\")\n",
+    "b=int(input(\"enter integer\"))\n",
+    "string(a,b)"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "ee73d40d",
+   "metadata": {},
+   "source": [
+    "#Q17. Write a code to take a number as an input from the\n",
+    "#user and check if the number is odd or even.\n",
+    "n=int(input(\"enter input\"))\n",
+    "if n%2 == 0:\n",
+    "    print(\"even\")\n",
+    "if n%2 != 0:\n",
+    "    print(\"odd\")\n"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "a0e31f78",
+   "metadata": {},
+   "source": [
+    "Q18. What are boolean operator?\n",
+    "\n",
+    "Boolean Operators are those that result in the Boolean values of True and False. These include and, or and not. While and & or require 2 operands, not is a unary operator. There are two types of operators in Python that return boolean values, i.e., Logical operators and Comparison operators. Since both these operators have return types as boolean, they are also termed, Boolean operators. Boolean operators are most commonly used in arithmetic computations and logical comparisons"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "a3cf086e",
+   "metadata": {},
+   "source": [
+    "#Q19. What will the output of the following?\n",
+    "1 or 0                   ans:1\n",
+    "\n",
+    "0 and 0                  ans:0\n",
+    "\n",
+    "True and False and True  ans:False\n",
+    "\n",
+    "1 or 0 or 0              ans:1"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "c8a2e517",
+   "metadata": {},
+   "source": [
+    "Q20. What are conditional statements in Python?\n",
+    "\n",
+    "A conditional statement as the name suggests itself, is used to handle conditions in your program. These statements guide the program while making decisions based on the conditions encountered by the program.\n",
+    "\n",
+    "Python has 3 key Conditional Statements:\n",
+    "\n",
+    "if statement\n",
+    "if-else statement\n",
+    "if-elif-else ladder"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "e64342e7",
+   "metadata": {},
+   "source": [
+    "Q21. What is use of 'if', 'elif' and 'else' keywords?\n",
+    " if ,elif and else keywords are used to satisfy some conditional statements in program.IF 'if' conditional statement is true then 'if' block code will be executed.otherwise it goes to 'elif'conditional statement if it satisfies condition then 'elif' block code will be executed or otherwise the 'else' block code is executed.all are used for decision making."
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "bdabd981",
+   "metadata": {},
+   "source": [
+    "#Q22. Write a code to take the age of person as\n",
+    "#an input and if age >= 18 display \"I can vote\". \n",
+    "#If age is < 18 display \"I can't vote\".\n",
+    "age = int(input(\"enter your age \"))\n",
+    "if age>=18:\n",
+    "    print(\"Ican vote \")\n",
+    "else:\n",
+    "    if age < 18:\n",
+    "        print(\"I can't  vote\")\n"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "625f6cb3",
+   "metadata": {},
+   "source": [
+    "#Q23. Write a code that displays the sum of all\n",
+    "#the even numbers from the given list.\n",
+    "#numbers = [12, 75, 150, 180, 145, 525, 50]\n",
+    "l= [12, 75, 150, 180, 145, 525, 50]\n",
+    "sum = 0\n",
+    "for i in range(len(l)):\n",
+    "    if l[i]%2 == 0:\n",
+    "        sum = sum +l[i]\n",
+    "print(sum)"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "1283499e",
+   "metadata": {},
+   "source": [
+    "#Q24. Write a code to take 3 numbers as an input from the user and\n",
+    "#display the greatest no as output.\n",
+    "\n",
+    "a = int(input(\"enter integer1\"))\n",
+    "b = int(input(\"enter integer2\"))\n",
+    "c = int(input(\"enter integer3\"))\n",
+    "if a>=b and a>=c:\n",
+    "    print(a)\n",
+    "elif b>=a and b>=c:\n",
+    "    print(b)\n",
+    "else:\n",
+    "    print(c)"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "e8d70a33",
+   "metadata": {},
+   "source": [
+    " #Q25. Write a program to display only those numbers from a list that\n",
+    "#satisfy the following conditions\n",
+    "\n",
+    "#The number must be divisible by five\n",
+    "\n",
+    "#If the number is greater than 150, then skip it and move to the next number\n",
+    "\n",
+    "#If the number is greater than 500, then stop the loop\n",
+    "#numbers = [12, 75, 150, 180, 145, 525, 50]\n",
+    "\n",
+    "\n",
+    "l =  [12, 75, 150, 180, 145, 525, 50]\n",
+    "i=0\n",
+    "while i<=len(l): \n",
+    "    if l[i]>500:\n",
+    "        break;\n",
+    "    if l[i]%5==0:\n",
+    "        if l[i]>150:\n",
+    "            i=i+1\n",
+    "        else:\n",
+    "            print(l[i])\n",
+    "            i=i+1\n",
+    "    else:\n",
+    "        i=i+1\n",
+    "          "
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "d713ba28",
+   "metadata": {},
+   "source": [
+    "Q26. What is a string? How can we declare string in Python?\n",
+    "\n",
+    "strings in Python are arrays of bytes representing unicode characters. However, Python does not have a character data type, a single character is simply a string with a length of 1. "
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "b28a5235",
+   "metadata": {},
+   "source": [
+    "Q27. How can we access the string using its index?\n",
+    "\n",
+    "Square brackets can be used to access elements of the string.\n",
+    "example:\n",
+    "s=\"elephant\"\n",
+    "to acces  characters \n",
+    "s[0],s[1],s[2]..ect.,\n"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "f1d129e6",
+   "metadata": {},
+   "source": [
+    "#Q28. Write a code to get the desired output of the following\n",
+    "#string = \"Big Data iNeuron\"\n",
+    "#desired_output = \"iNeuron\"\n",
+    "string = input()\n",
+    "x=string.split()\n",
+    "for i in range(len(x)):\n",
+    "    if x[i] == \"iNeuron\":\n",
+    "        print(\"desired_output\",x[i])"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "91ec36b5",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fa862271",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.13"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
